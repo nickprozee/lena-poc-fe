1355ddc1ef6467864c8790dcd127bd877aa54957
nick <NickProzee_Development@outlook.com>
2023-07-10 11:58:56 +0200

    added mock server for responses in json

---
+++ mock.middleware.ts
@@ -0,0 +1,39 @@
+let summarize_processed = false
+
+module.exports = function (req, res, next) {
+    const METHOD = req.method
+    const PATH = req.path
+
+    const MethodNotAllowed = () => {
+        res.status(405).send()
+    }
+
+    const ProcessingSummarize = () => {
+        res.status(202).send()
+    }
+
+    if (req.path === '/create') {
+        if (METHOD !== 'POST') return MethodNotAllowed()
+
+        req.method = 'GET'
+        req.query = req.body
+    }
+
+    if (PATH.indexOf('/upload') > -1 && METHOD !== 'PUT')
+        return MethodNotAllowed()
+
+    if (PATH.indexOf('/summarize') > -1) {
+        if (METHOD !== 'GET') return MethodNotAllowed()
+
+        if (!summarize_processed) {
+            summarize_processed = !summarize_processed
+            return ProcessingSummarize();
+        }
+
+    }
+
+    console.log(req)
+
+    // Continue to JSON Server router
+    next()
+}
