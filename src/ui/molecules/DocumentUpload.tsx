69596e7f9de08e4b62ad9fe8b40d4a463955b517
nick <NickProzee_Development@outlook.com>
2023-07-11 10:08:18 +0200

    added file types filter

--- src/ui/molecules/DocumentUpload.tsx
+++ src/ui/molecules/DocumentUpload.tsx
@@ -17,7 +17,7 @@ interface HiddenProps {
 
 export function UploadArea(props: HiddenProps) {
     return (
-        <FileUploader multiple={false} handleChange={props.onUpload}>
+        <FileUploader multiple={false} handleChange={props.onUpload} types={['PDF', 'ZIP']}>
             {props.children}
         </FileUploader>
     )
