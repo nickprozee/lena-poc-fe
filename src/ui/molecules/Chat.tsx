d13769170fd9ceaa5758798c73f29a17695aaa5f
nick <NickProzee_Development@outlook.com>
2023-07-11 02:50:36 +0200

    UI improvfements

--- src/ui/molecules/Chat.tsx
+++ src/ui/molecules/Chat.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { InvestigationViewModel } from '../../types/Investigations'
 import { getAsset } from '../../utils/assetHelper'
 import { Container, Text, Image } from '../atoms'
@@ -6,16 +7,41 @@ interface Props {
     investigation: InvestigationViewModel
 }
 
+let index = 0
+const messages = [
+    'Geheimen ontgrendelen..',
+    'Document decrypten...',
+    'Vertalen naar morse-code...',
+]
+
 export function Chat(props: Props) {
-    const { state, title, summary } = props.investigation
+    const { title, summary } = props.investigation
+
+    useEffect(() => {
+        if (index < 2) index++
+        else index = 0
+    }, [props.investigation.id])
 
     return (
-        <Container centered sx={{ height: '100vh' }}>
-            <Text size="title" value={title} bold />
-            <Text size="title" value={state} />
-
-            {summary && <Text size="subtitle" value={summary} />}
-            {!summary && <Image src={getAsset('document_scan.gif')} sx={{maxWidth: 100, borderRadius: 60, mt: 5}} />}
+        <Container
+            key={props.investigation.id}
+            centered
+            sx={{ height: '100vh' }}>
+            {summary && (
+                <>
+                    <Text size="title" value={title} bold />
+                    <Text size="subtitle" value={summary} />
+                </>
+            )}
+            {!summary && (
+                <>
+                    <Image
+                        src={getAsset('document_scan.gif')}
+                        sx={{ maxWidth: 100, borderRadius: 60, mt: 5 }}
+                    />
+                    <Text size="subtitle" value={messages[index]} />
+                </>
+            )}
         </Container>
     )
 }
